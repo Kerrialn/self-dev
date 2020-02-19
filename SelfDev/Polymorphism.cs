c2522172cfde8463d75e904d5142585d7648541f
Your Name <you@example.com>
2020-02-19 10:45:23 +0100

    rename folders

--- SelfDev/Polymorphism.cs
+++ SelfDev/Polymorphism.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace PaymentSystem
+namespace SelfDev
 {
 
     class Shape
--- SelfDev/Program.cs
+++ SelfDev/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace PaymentSystem
+namespace SelfDev
 {
     class Program
     {
